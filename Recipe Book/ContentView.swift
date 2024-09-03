7608166c65d383d449dfe68d7a31613eba95bcd5
Josh Birnholz <josh@birnholz.com>
2024-09-03 16:48:52 -0400

    Initial Commit

---
+++ Recipe Book/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipe Book
+//
+//  Created by Josh Birnholz on 9/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recipe Book/Recipe_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_BookApp.swift
+//  Recipe Book
+//
+//  Created by Josh Birnholz on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
